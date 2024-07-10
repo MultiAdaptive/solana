8f0f9e43a9bb9546a9142988d3e58881295c31f4
simbahebinbo <lansheng228@163.com>
2024-07-10 20:15:29 +0800

    add postgresql plugin

--- solana-accountsdb-plugin-postgres/scripts/create_schema.sql
+++ solana-accountsdb-plugin-postgres/scripts/create_schema.sql
@@ -4,17 +4,19 @@
 -- The table storing accounts
 
 
-CREATE TABLE account (
-    pubkey BYTEA PRIMARY KEY,
-    owner BYTEA,
-    lamports BIGINT NOT NULL,
-    slot BIGINT NOT NULL,
-    executable BOOL NOT NULL,
-    rent_epoch BIGINT NOT NULL,
-    data BYTEA,
-    write_version BIGINT NOT NULL,
-    updated_on TIMESTAMP NOT NULL,
-    txn_signature BYTEA
+CREATE TABLE account
+(
+    id            bigserial PRIMARY KEY,
+    pubkey        BYTEA UNIQUE,
+    owner         BYTEA,
+    lamports      BIGINT    NOT NULL,
+    slot          BIGINT    NOT NULL,
+    executable    BOOL      NOT NULL,
+    rent_epoch    BIGINT    NOT NULL,
+    data          BYTEA,
+    write_version BIGINT    NOT NULL,
+    txn_signature BYTEA,
+    updated_on    TIMESTAMP NOT NULL
 );
 
 CREATE INDEX account_owner ON account (owner);
@@ -22,12 +24,23 @@ CREATE INDEX account_owner ON account (owner);
 CREATE INDEX account_slot ON account (slot);
 
 -- The table storing slot information
-CREATE TABLE slot (
-    slot BIGINT PRIMARY KEY,
-    parent BIGINT,
-    status VARCHAR(16) NOT NULL,
+CREATE TABLE slot
+(
+    slot       BIGINT PRIMARY KEY,
+    parent     BIGINT,
+    status     VARCHAR(16) NOT NULL,
+    updated_on TIMESTAMP   NOT NULL
+);
+
+
+CREATE TABLE merkle_tree_proof
+(
+    slot       BIGINT    NOT NULL,
+    root_hash  VARCHAR(256),
     updated_on TIMESTAMP NOT NULL
 );
+
+CREATE INDEX merkle_tree_proof_slot_index ON merkle_tree_proof (slot);
 
 -- Types for Transactions
 
@@ -70,29 +83,33 @@ Create TYPE "TransactionErrorCode" AS ENUM (
     'ResanitizationNeeded',
     'UnbalancedTransaction',
     'ProgramExecutionTemporarilyRestricted'
-);
+    );
 
-CREATE TYPE "TransactionError" AS (
-    error_code "TransactionErrorCode",
+CREATE TYPE "TransactionError" AS
+(
+    error_code   "TransactionErrorCode",
     error_detail VARCHAR(256)
 );
 
-CREATE TYPE "CompiledInstruction" AS (
+CREATE TYPE "CompiledInstruction" AS
+(
     program_id_index SMALLINT,
-    accounts SMALLINT[],
-    data BYTEA
+    accounts         SMALLINT[],
+    data             BYTEA
 );
 
-CREATE TYPE "InnerInstructions" AS (
-    index SMALLINT,
+CREATE TYPE "InnerInstructions" AS
+(
+    index        SMALLINT,
     instructions "CompiledInstruction"[]
 );
 
-CREATE TYPE "TransactionTokenBalance" AS (
-    account_index SMALLINT,
-    mint VARCHAR(44),
+CREATE TYPE "TransactionTokenBalance" AS
+(
+    account_index   SMALLINT,
+    mint            VARCHAR(44),
     ui_token_amount DOUBLE PRECISION,
-    owner VARCHAR(44)
+    owner           VARCHAR(44)
 );
 
 Create TYPE "RewardType" AS ENUM (
@@ -100,144 +117,196 @@ Create TYPE "RewardType" AS ENUM (
     'Rent',
     'Staking',
     'Voting'
-);
+    );
 
-CREATE TYPE "Reward" AS (
-    pubkey VARCHAR(44),
-    lamports BIGINT,
+CREATE TYPE "Reward" AS
+(
+    pubkey       VARCHAR(44),
+    lamports     BIGINT,
     post_balance BIGINT,
-    reward_type "RewardType",
-    commission SMALLINT
+    reward_type  "RewardType",
+    commission   SMALLINT
 );
 
-CREATE TYPE "TransactionStatusMeta" AS (
-    error "TransactionError",
-    fee BIGINT,
-    pre_balances BIGINT[],
-    post_balances BIGINT[],
-    inner_instructions "InnerInstructions"[],
-    log_messages TEXT[],
-    pre_token_balances "TransactionTokenBalance"[],
+CREATE TYPE "TransactionStatusMeta" AS
+(
+    error               "TransactionError",
+    fee                 BIGINT,
+    pre_balances        BIGINT[],
+    post_balances       BIGINT[],
+    inner_instructions  "InnerInstructions"[],
+    log_messages        TEXT[],
+    pre_token_balances  "TransactionTokenBalance"[],
     post_token_balances "TransactionTokenBalance"[],
-    rewards "Reward"[]
+    rewards             "Reward"[]
 );
 
-CREATE TYPE "TransactionMessageHeader" AS (
-    num_required_signatures SMALLINT,
-    num_readonly_signed_accounts SMALLINT,
+CREATE TYPE "TransactionMessageHeader" AS
+(
+    num_required_signatures        SMALLINT,
+    num_readonly_signed_accounts   SMALLINT,
     num_readonly_unsigned_accounts SMALLINT
 );
 
-CREATE TYPE "TransactionMessage" AS (
-    header "TransactionMessageHeader",
-    account_keys BYTEA[],
+CREATE TYPE "TransactionMessage" AS
+(
+    header           "TransactionMessageHeader",
+    account_keys     BYTEA[],
     recent_blockhash BYTEA,
-    instructions "CompiledInstruction"[]
+    instructions     "CompiledInstruction"[]
 );
 
-CREATE TYPE "TransactionMessageAddressTableLookup" AS (
-    account_key BYTEA,
+CREATE TYPE "TransactionMessageAddressTableLookup" AS
+(
+    account_key      BYTEA,
     writable_indexes SMALLINT[],
     readonly_indexes SMALLINT[]
 );
 
-CREATE TYPE "TransactionMessageV0" AS (
-    header "TransactionMessageHeader",
-    account_keys BYTEA[],
-    recent_blockhash BYTEA,
-    instructions "CompiledInstruction"[],
+CREATE TYPE "TransactionMessageV0" AS
+(
+    header                "TransactionMessageHeader",
+    account_keys          BYTEA[],
+    recent_blockhash      BYTEA,
+    instructions          "CompiledInstruction"[],
     address_table_lookups "TransactionMessageAddressTableLookup"[]
 );
 
-CREATE TYPE "LoadedAddresses" AS (
+CREATE TYPE "LoadedAddresses" AS
+(
     writable BYTEA[],
     readonly BYTEA[]
 );
 
-CREATE TYPE "LoadedMessageV0" AS (
-    message "TransactionMessageV0",
+CREATE TYPE "LoadedMessageV0" AS
+(
+    message          "TransactionMessageV0",
     loaded_addresses "LoadedAddresses"
 );
 
 -- The table storing transactions
-CREATE TABLE transaction (
-    slot BIGINT NOT NULL,
-    signature BYTEA NOT NULL,
-    is_vote BOOL NOT NULL,
-    message_type SMALLINT, -- 0: legacy, 1: v0 message
-    legacy_message "TransactionMessage",
+CREATE TABLE transaction
+(
+    slot              BIGINT    NOT NULL,
+    signature         BYTEA     NOT NULL,
+    is_vote           BOOL      NOT NULL,
+    message_type      SMALLINT, -- 0: legacy, 1: v0 message
+    legacy_message    "TransactionMessage",
     v0_loaded_message "LoadedMessageV0",
-    signatures BYTEA[],
-    message_hash BYTEA,
-    meta "TransactionStatusMeta",
-    write_version BIGINT,
-    updated_on TIMESTAMP NOT NULL,
-    index BIGINT NOT NULL,
+    signatures        BYTEA[],
+    message_hash      BYTEA,
+    meta              "TransactionStatusMeta",
+    write_version     BIGINT,
+    updated_on        TIMESTAMP NOT NULL,
+    index             BIGINT    NOT NULL,
     CONSTRAINT transaction_pk PRIMARY KEY (slot, signature)
 );
 
 -- The table storing block metadata
-CREATE TABLE block (
-    slot BIGINT PRIMARY KEY,
-    blockhash VARCHAR(44),
-    rewards "Reward"[],
-    block_time BIGINT,
+CREATE TABLE block
+(
+    slot         BIGINT PRIMARY KEY,
+    blockhash    VARCHAR(44),
+    rewards      "Reward"[],
+    block_time   BIGINT,
     block_height BIGINT,
-    updated_on TIMESTAMP NOT NULL
+    updated_on   TIMESTAMP NOT NULL
 );
 
 -- The table storing spl token owner to account indexes
-CREATE TABLE spl_token_owner_index (
-    owner_key BYTEA NOT NULL,
-    account_key BYTEA NOT NULL,
-    slot BIGINT NOT NULL
+CREATE TABLE spl_token_owner_index
+(
+    owner_key   BYTEA  NOT NULL,
+    account_key BYTEA  NOT NULL,
+    slot        BIGINT NOT NULL
 );
 
 CREATE INDEX spl_token_owner_index_owner_key ON spl_token_owner_index (owner_key);
 CREATE UNIQUE INDEX spl_token_owner_index_owner_pair ON spl_token_owner_index (owner_key, account_key);
 
 -- The table storing spl mint to account indexes
-CREATE TABLE spl_token_mint_index (
-    mint_key BYTEA NOT NULL,
-    account_key BYTEA NOT NULL,
-    slot BIGINT NOT NULL
+CREATE TABLE spl_token_mint_index
+(
+    mint_key    BYTEA  NOT NULL,
+    account_key BYTEA  NOT NULL,
+    slot        BIGINT NOT NULL
 );
 
 CREATE INDEX spl_token_mint_index_mint_key ON spl_token_mint_index (mint_key);
 CREATE UNIQUE INDEX spl_token_mint_index_mint_pair ON spl_token_mint_index (mint_key, account_key);
 
+CREATE TABLE IF NOT EXISTS entry
+(
+    id           bigserial PRIMARY KEY,
+    slot         BIGINT    NOT NULL,
+    parent_slot  BIGINT    NOT NULL,
+    entry_index  BIGINT    NOT NULL,
+    entry        BYTEA,
+    is_full_slot BOOL      NOT NULL,
+    updated_on   TIMESTAMP NOT NULL
+);
+
+CREATE INDEX entry_slot_index ON entry (slot, entry_index);
+
+
+
 /**
  * The following is for keeping historical data for accounts and is not required for plugin to work.
  */
 -- The table storing historical data for accounts
-CREATE TABLE account_audit (
-    pubkey BYTEA,
-    owner BYTEA,
-    lamports BIGINT NOT NULL,
-    slot BIGINT NOT NULL,
-    executable BOOL NOT NULL,
-    rent_epoch BIGINT NOT NULL,
-    data BYTEA,
-    write_version BIGINT NOT NULL,
-    updated_on TIMESTAMP NOT NULL,
-    txn_signature BYTEA
+CREATE TABLE account_audit
+(
+    id            bigserial PRIMARY KEY,
+    pubkey        BYTEA     NOT NULL,
+    owner         BYTEA,
+    lamports      BIGINT    NOT NULL,
+    slot          BIGINT    NOT NULL,
+    executable    BOOL      NOT NULL,
+    rent_epoch    BIGINT    NOT NULL,
+    data          BYTEA,
+    write_version BIGINT    NOT NULL,
+    txn_signature BYTEA,
+    updated_on    TIMESTAMP NOT NULL
 );
 
-CREATE INDEX account_audit_account_key ON  account_audit (pubkey, write_version);
+CREATE INDEX account_audit_account_key ON account_audit (pubkey, write_version);
 
 CREATE INDEX account_audit_pubkey_slot ON account_audit (pubkey, slot);
 
-CREATE FUNCTION audit_account_update() RETURNS trigger AS $audit_account_update$
-    BEGIN
-		INSERT INTO account_audit (pubkey, owner, lamports, slot, executable,
-		                           rent_epoch, data, write_version, updated_on, txn_signature)
-            VALUES (OLD.pubkey, OLD.owner, OLD.lamports, OLD.slot,
-                    OLD.executable, OLD.rent_epoch, OLD.data,
-                    OLD.write_version, OLD.updated_on, OLD.txn_signature);
-        RETURN NEW;
-    END;
+CREATE FUNCTION account_modify() RETURNS TRIGGER AS
+$account_modify$
+BEGIN
+    INSERT INTO account_audit (pubkey, owner, lamports, slot, executable,
+                               rent_epoch, data, write_version, updated_on, txn_signature)
+    VALUES (NEW.pubkey, NEW.owner, NEW.lamports, NEW.slot,
+            NEW.executable, NEW.rent_epoch, NEW.data,
+            NEW.write_version, NEW.updated_on, NEW.txn_signature);
 
-$audit_account_update$ LANGUAGE plpgsql;
+    RETURN NEW;
+END;
+$account_modify$ language plpgsql;
 
-CREATE TRIGGER account_update_trigger AFTER UPDATE OR DELETE ON account
-    FOR EACH ROW EXECUTE PROCEDURE audit_account_update();
+CREATE TRIGGER account_modify_trigger
+    AFTER INSERT OR UPDATE OR DELETE
+    ON account
+    FOR EACH ROW
+EXECUTE PROCEDURE account_modify();
+
+
+CREATE TABLE IF NOT EXISTS brief
+(
+    id                 bigserial PRIMARY KEY,
+    slot               BIGINT UNIQUE NOT NULL,
+    root_hash          VARCHAR(256)  NOT NULL,
+    hash_account       VARCHAR(256)  NOT NULL,
+    transaction_number INT           NOT NULL,
+    updated_on         timestamp default current_timestamp
+);
+
+
+CREATE TABLE IF NOT EXISTS genesis
+(
+    id           bigserial PRIMARY KEY,
+    genesis_hash VARCHAR(64) UNIQUE NOT NULL,
+    updated_on   timestamp default current_timestamp
+);
--- solana-accountsdb-plugin-postgres/scripts/drop_schema.sql
+++ solana-accountsdb-plugin-postgres/scripts/drop_schema.sql
@@ -2,8 +2,8 @@
  * Script for cleaning up the schema for PostgreSQL used for the AccountsDb plugin.
  */
 
-DROP TRIGGER account_update_trigger ON account;
-DROP FUNCTION audit_account_update;
+DROP TRIGGER account_modify_trigger ON account;
+DROP FUNCTION account_modify;
 DROP TABLE account_audit;
 DROP TABLE account CASCADE;
 DROP TABLE slot;
@@ -11,7 +11,12 @@ DROP TABLE transaction;
 DROP TABLE block;
 DROP TABLE spl_token_owner_index;
 DROP TABLE spl_token_mint_index;
+DROP TABLE entry;
+DROP TABLE merkle_tree_proof;
+DROP TABLE genesis;
+DROP TABLE brief;
 
+DROP TYPE "TransactionStatusMeta" CASCADE;
 DROP TYPE "TransactionError" CASCADE;
 DROP TYPE "TransactionErrorCode" CASCADE;
 DROP TYPE "LoadedMessageV0" CASCADE;
@@ -20,9 +25,8 @@ DROP TYPE "TransactionMessageV0" CASCADE;
 DROP TYPE "TransactionMessage" CASCADE;
 DROP TYPE "TransactionMessageHeader" CASCADE;
 DROP TYPE "TransactionMessageAddressTableLookup" CASCADE;
-DROP TYPE "TransactionStatusMeta" CASCADE;
-DROP TYPE "RewardType" CASCADE;
 DROP TYPE "Reward" CASCADE;
+DROP TYPE "RewardType" CASCADE;
 DROP TYPE "TransactionTokenBalance" CASCADE;
 DROP TYPE "InnerInstructions" CASCADE;
 DROP TYPE "CompiledInstruction" CASCADE;
