3b2760536a3c6f6b1690fa4b70eadd5a01260145
simbahebinbo <lansheng228@163.com>
2024-07-12 11:25:12 +0800

    add fraud proof

--- fraud-proof/src/contract/mod.rs
+++ fraud-proof/src/contract/mod.rs
@@ -1,6 +1,4 @@
 pub mod chain_tally;
-
 pub mod chain_brief;
-
 pub mod wrap_slot;
 pub mod wrap_key;
--- fraud-proof/src/lib.rs
+++ fraud-proof/src/lib.rs
@@ -1,7 +1,7 @@
-// pub mod simulator;
+pub mod simulator;
 pub mod common;
 pub mod utils;
 pub mod entities;
 pub mod model;
 // pub mod services;
-// pub mod fraud_proof;
+pub mod contract;
