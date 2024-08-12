ea52ed26369fbb385957ee0b9a39a1789b16b4fc
simbahebinbo <lansheng228@163.com>
2024-08-12 11:50:29 +0800

    add postgresql plugin

--- accountsdb-plugin-postgres/scripts/drop_schema.sql
+++ accountsdb-plugin-postgres/scripts/drop_schema.sql
@@ -2,9 +2,12 @@
  * Script for cleaning up the schema for PostgreSQL used for the AccountsDb plugin.
  */
 
-DROP TRIGGER account_update_trigger ON account;
+DROP TRIGGER audit_account_update_trigger ON account;
 DROP FUNCTION audit_account_update;
 DROP TABLE account_audit;
+DROP TRIGGER inspect_account_update_trigger ON account;
+DROP FUNCTION inspect_account_update;
+DROP TABLE account_inspect;
 DROP TABLE account CASCADE;
 DROP TABLE slot;
 DROP TABLE transaction;
